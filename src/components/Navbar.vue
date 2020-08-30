6b5ec8cab2cfd59a65d5940bcbf2a601839dd7c6
Neenus Gabriel <neenus@live.ca>
2020-08-30 04:21:41 -0400

    removed boilerplate code and added a navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="navbar">
+    <v-app-bar app>
+      <v-spacer></v-spacer>
+      <div class="text-h4 font-weight-black indigo--text text--darken-4">
+        The Shoppies
+      </div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
