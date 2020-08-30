0463c6ac98fa5fe6c8a65ca872356e3010c7671e
Neenus Gabriel <neenus@live.ca>
2020-08-30 04:34:37 -0400

    installed material design icons and added search component

---
+++ src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="search">
+    <v-row>
+      <v-col cols="12" md="8" offset-md="2">
+        <v-card flat prominent class="mt-5">
+          <v-text-field
+            height="50px"
+            class="mx-4"
+            flat
+            hide-details
+            label="Search"
+            prepend-inner-icon="search"
+            solo-inverted
+          ></v-text-field>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <v-container>
+      <Search />
+    </v-container>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Search from "@/components/Search";
 
 export default {
   name: "Home",
   components: {
-    //
+    Search
   }
 };
 </script>
