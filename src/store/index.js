8453ffa51833f0f1e29aa767e5da019f11fd3185
Neenus Gabriel <neenus@live.ca>
2020-08-30 21:54:23 -0400

    moved mock data from home view to vuex store for better state management

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,82 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     searchTerm: null,
-    searchResults: [],
-    nominationsList: []
+    searchResults: [
+      {
+        Title: "Fast & Furious 6",
+        Year: "2013",
+        imdbID: "tt1905041",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg"
+      },
+      {
+        Title: "Fast Five",
+        Year: "2011",
+        imdbID: "tt1596343",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTUxNTk5MTE0OF5BMl5BanBnXkFtZTcwMjA2NzY3NA@@._V1_SX300.jpg"
+      },
+      {
+        Title: "The Fast and the Furious",
+        Year: "2001",
+        imdbID: "tt0232500",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+      },
+      {
+        Title: "Fast & Furious",
+        Year: "2009",
+        imdbID: "tt1013752",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+      },
+      {
+        Title: "2 Fast 2 Furious",
+        Year: "2003",
+        imdbID: "tt0322259",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMzExYjcyYWMtY2JkOC00NDUwLTg2OTgtMDI3MGY2OWQzMDE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+      },
+      {
+        Title: "The Fast and the Furious: Tokyo Drift",
+        Year: "2006",
+        imdbID: "tt0463985",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTQ2NTMxODEyNV5BMl5BanBnXkFtZTcwMDgxMjA0MQ@@._V1_SX300.jpg"
+      },
+      {
+        Title: "Fast & Furious Presents: Hobbs & Shaw",
+        Year: "2019",
+        imdbID: "tt6806448",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+      }
+    ],
+    nominationsList: [
+      {
+        Title: "Fast & Furious Presents: Hobbs & Shaw",
+        Year: "2019",
+        imdbID: "tt6806448",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
+      },
+      {
+        Title: "Fast & Furious 6",
+        Year: "2013",
+        imdbID: "tt1905041",
+        Type: "movie",
+        Poster:
+          "https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg"
+      }
+    ]
   },
   mutations: {
     SEARCH(state, payload) {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
               "
             ></v-card-title>
             <v-list>
-              <template v-for="(result, index) in results">
+              <template v-for="(result, index) in searchResults">
                 <v-divider class="my-2" :key="index"></v-divider>
                 <v-list-item :key="result.Title">
                   <v-img
@@ -57,7 +57,7 @@
             <!-- nomination list -->
             <v-card-title class="justify-center">Nomiations:</v-card-title>
             <v-list>
-              <template v-for="(item, index) in nominatedList">
+              <template v-for="(item, index) in nominationsList">
                 <v-divider class="my-2" :key="index"></v-divider>
                 <v-list-item :key="item.Title">
                   <v-img
@@ -100,110 +100,10 @@ export default {
   components: {
     Search
   },
-  computed: mapState(["searchTerm"]),
+  computed: mapState(["searchTerm", "nominationsList", "searchResults"]),
   data() {
     return {
-      mobile: false,
-      results: [
-        {
-          Title: "Fast & Furious 6",
-          Year: "2013",
-          imdbID: "tt1905041",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast Five",
-          Year: "2011",
-          imdbID: "tt1596343",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BMTUxNTk5MTE0OF5BMl5BanBnXkFtZTcwMjA2NzY3NA@@._V1_SX300.jpg"
-        },
-        {
-          Title: "The Fast and the Furious",
-          Year: "2001",
-          imdbID: "tt0232500",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast & Furious",
-          Year: "2009",
-          imdbID: "tt1013752",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BYjQ1ZTMxNzgtZDcxOC00NWY5LTk3ZjAtYzRhMDhlNDZlOWEzXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-          Title: "2 Fast 2 Furious",
-          Year: "2003",
-          imdbID: "tt0322259",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BMzExYjcyYWMtY2JkOC00NDUwLTg2OTgtMDI3MGY2OWQzMDE2XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-        },
-        {
-          Title: "The Fast and the Furious: Tokyo Drift",
-          Year: "2006",
-          imdbID: "tt0463985",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BMTQ2NTMxODEyNV5BMl5BanBnXkFtZTcwMDgxMjA0MQ@@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast & Furious Presents: Hobbs & Shaw",
-          Year: "2019",
-          imdbID: "tt6806448",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast Times at Ridgemont High",
-          Year: "1982",
-          imdbID: "tt0083929",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BYzBlZjE1MDctYjZmZC00ZTJmLWFkOWEtYjdmZDZkODBkZmI2XkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast Food Nation",
-          Year: "2006",
-          imdbID: "tt0460792",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BYzM0ODQ5OGItZDZhMy00NDk0LWJmNTgtZGRiMTBjMDI1YTAwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
-        },
-        {
-          Title: "How to Sell Drugs Online (Fast)",
-          Year: "2019",
-          imdbID: "tt9184994",
-          Type: "series",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BZjIzNTkyNjktNDdmMy00MmI4LWFlN2MtNDllYTVhZjljNWFiXkEyXkFqcGdeQXVyMTAwMzM3NDI3._V1_SX300.jpg"
-        }
-      ],
-      nominatedList: [
-        {
-          Title: "Fast & Furious Presents: Hobbs & Shaw",
-          Year: "2019",
-          imdbID: "tt6806448",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BOTIzYmUyMmEtMWQzNC00YzExLTk3MzYtZTUzYjMyMmRiYzIwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"
-        },
-        {
-          Title: "Fast & Furious 6",
-          Year: "2013",
-          imdbID: "tt1905041",
-          Type: "movie",
-          Poster:
-            "https://m.media-amazon.com/images/M/MV5BMTM3NTg2NDQzOF5BMl5BanBnXkFtZTcwNjc2NzQzOQ@@._V1_SX300.jpg"
-        }
-      ]
+      mobile: false
     };
   },
   mounted() {
