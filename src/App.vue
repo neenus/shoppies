dcb90cb67355578cac4a47d0c2c9dfb6449fc102
Neenus Gabriel <neenus@live.ca>
2021-07-23 01:25:42 -0400

    update meta tags example

--- src/App.vue
+++ src/App.vue
@@ -13,26 +13,42 @@ import Navbar from "./components/Navbar";
 export default {
   name: "App",
   data: () => ({
-    logo: require("./assets/logo.png")
+    //
   }),
   metaInfo: {
-    title: "Shoppies",
     meta: [
+      { name: "title", content: "Shoppies" },
+      {
+        name: "description",
+        content:
+          "The shoppies is a movie search and nomination single page application built with Vue CLI, Vuex and Vueitfy using the OMDb API (The Open Movie Database)."
+      },
+
       // Twitter Card
-      { name: "twitter:card", content: "summary" },
+      { name: "twitter:card", content: "summary_large_image" },
       { name: "twitter:title", content: "Shoppies" },
       {
         name: "twitter:description",
         content:
           "The shoppies is a movie search and nomination single page application built with Vue CLI, Vuex and Vueitfy using the OMDb API (The Open Movie Database)."
       },
-      { name: "twitter:image", content: require("./assets/logo.png") },
+      {
+        name: "twitter:image",
+        content: "https://cdn.shopify.com/assets/images/logos/shopify-bag.png"
+      },
 
       // Facebook OpenGraph
+      {
+        property: "og:url",
+        content: "https://shoppies.neenus.com/"
+      },
       { property: "og:title", content: "Shoppies" },
       { property: "og:site_name", content: "Shoppies" },
       { property: "og:type", content: "website" },
-      { property: "og:image", content: require("./assets/logo.png") },
+      {
+        property: "og:image",
+        content: "https://cdn.shopify.com/assets/images/logos/shopify-bag.png"
+      },
       {
         property: "og:description",
         content:
