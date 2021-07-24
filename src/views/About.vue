3df7a3e729e5a5e4a72d630f8953c05cbfffbd9e
Neenus Gabriel <neenus@live.ca>
2021-07-24 14:30:24 -0400

    update metaInfo on about page to test sharing links showing different meta tags

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,45 @@
 export default {
   name: "About",
   metaInfo: {
-    title: "Shoppies | About"
+    meta: [
+      { name: "title", content: "Shoppies | About" },
+      {
+        name: "description",
+        content:
+          "The shoppies is a movie search and nomination single page application built with Vue CLI, Vuex and Vueitfy using the OMDb API (The Open Movie Database)."
+      },
+
+      // Twitter Card
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:title", content: "Shoppies" },
+      {
+        name: "twitter:description",
+        content:
+          "The shoppies is a movie search and nomination single page application built with Vue CLI, Vuex and Vueitfy using the OMDb API (The Open Movie Database)."
+      },
+      {
+        name: "twitter:image",
+        content: "https://cdn.cdnlogo.com/logos/a/94/about-me.svg"
+      },
+
+      // Facebook OpenGraph
+      {
+        property: "og:url",
+        content: "https://shoppies.neenus.com/"
+      },
+      { property: "og:title", content: "Shoppies" },
+      { property: "og:site_name", content: "Shoppies" },
+      { property: "og:type", content: "website" },
+      {
+        property: "og:image",
+        content: "https://cdn.cdnlogo.com/logos/a/94/about-me.svg"
+      },
+      {
+        property: "og:description",
+        content:
+          "The shoppies is a movie search and nomination single page application built with Vue CLI, Vuex and Vueitfy using the OMDb API (The Open Movie Database)."
+      }
+    ]
   }
 };
 </script>
