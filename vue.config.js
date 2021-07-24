6e67517f73e8626b5dd09225b40dcd04f808ab05
Neenus Gabriel <neenus@live.ca>
2021-07-24 14:15:59 -0400

    install and configure prerender-spa to test meta-tags crawling

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  transpileDependencies: ["vuetify", "vuex-persist"]
+  transpileDependencies: ["vuetify", "vuex-persist"],
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ["/", "/about"],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 };
